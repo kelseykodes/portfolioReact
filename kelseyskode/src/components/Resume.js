d88dc22c08b095fd9623f85f4ddf5e6b3e664078
kperkins97 <perkins.kelsey@yahoo.com>
2022-01-23 13:23:15 -0600

    trying to fix resume render

--- kelseyskode/src/components/Resume.js
+++ kelseyskode/src/components/Resume.js
@@ -1,5 +1,24 @@
 import React from 'react';
+import resume from '../images/resume.png';
+import ReactCrop from 'react-image-crop';
+import 'react-image-crop/dist/ReactCrop.css';
 
+class DownloadLink extends React.Component {
+  render() {
+      return (
+          <a href={this.props.src} download>{this.props.children}</a>
+      )
+  }
+}
+
+
+class MyComponent extends React.Component {
+  render() {
+      return (
+          <DownloadLink src="./MyDevResume.pdf">Click To Dowload and View!</DownloadLink>
+      )
+  }
+}
 const styles = {
   card: {
     margin: 20,
@@ -13,12 +32,21 @@ const styles = {
     color: 'white',
     padding: '0 20px',
   },
+  pic: {
+    crop: {
+      unit: '%',
+      width: 20,
+      aspect: 16 / 9
+    }
+  },
 };
 //TODO: Need to add photo of resume and downloadable
 function Resume() {
   return (
     <div style={styles.card} id="resume">
       <h2 style={styles.heading}>My Resume</h2>
+      <MyComponent />
+      <img style={styles.pic}src={ resume } alt="my resume"/>
     </div>
   );
 }
