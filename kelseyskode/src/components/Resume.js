16a49dacb844b2c4c5678fb2dfaad9c7a32afb51
kperkins97 <perkins.kelsey@yahoo.com>
2022-01-25 15:30:44 -0600

    installed packages

--- kelseyskode/src/components/Resume.js
+++ kelseyskode/src/components/Resume.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import resume from '../images/resume.png';
-import ReactCrop from 'react-image-crop';
+// import ReactCrop from 'react-image-crop';
 import 'react-image-crop/dist/ReactCrop.css';
+import DownloadLink from 'react-download-link';
+import { Link } from 'react-router-dom';
 
-class DownloadLink extends React.Component {
-  render() {
-      return (
-          <a href={this.props.src} download>{this.props.children}</a>
-      )
-  }
-}
+// class DownloadLink extends React.Component {
+//   render() {
+//       return (
+//           <a href={this.props.src} download>{this.props.children}</a>
+//       )
+//   }
+// }
 
 
-class MyComponent extends React.Component {
-  render() {
-      return (
-          <DownloadLink src="./MyDevResume.pdf">Click To Dowload and View!</DownloadLink>
-      )
-  }
-}
+// // class MyComponent extends React.Component {
+//   render() {
+//       return (
+//           <DownloadLink src="./mydevresume.pdf">Click To Download and View!</DownloadLink>
+//       )
+//   }
+// }
 const styles = {
   card: {
     margin: 20,
@@ -35,7 +37,7 @@ const styles = {
   pic: {
     crop: {
       unit: '%',
-      width: 20,
+      width: 200,
       aspect: 16 / 9
     }
   },
@@ -45,8 +47,9 @@ function Resume() {
   return (
     <div style={styles.card} id="resume">
       <h2 style={styles.heading}>My Resume</h2>
-      <MyComponent />
-      <img style={styles.pic}src={ resume } alt="my resume"/>
+      {/* <Link to='/mydevresume.pdf' target="_blank" download> Download Here!</Link> */}
+      {/* <Link href="/mydevresume.pdf" download="mydevresume.pdf">Download</Link> */}
+      <img style={styles.pic.crop}src={ resume } alt="my resume"/>
     </div>
   );
 }
